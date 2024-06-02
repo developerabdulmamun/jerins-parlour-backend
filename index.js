60533f27e888329fd33c8c31a934eb03b2134701
developerabdulmamun <abdulmamun.dev@gmail.com>
2024-06-02 14:31:38 +0600

    Add verify token and verify admin middlewares in the server

--- index.js
+++ index.js
@@ -44,8 +44,34 @@ async function run() {
       res.send({ token });
     });
 
-    // Middlewares 
-    
+    // Middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+
+      if (isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      next();
+    };
 
     // Users related api
     app.get("/users", async (req, res) => {
