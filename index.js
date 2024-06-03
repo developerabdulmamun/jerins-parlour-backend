0269dd5b37201d958af8cd01dd14ec8f3d7b668b
developerabdulmamun <abdulmamun.dev@gmail.com>
2024-06-03 09:19:09 +0600

    Update mongoDB client connect for vercel deploy error

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const usersCollection = client.db("parlourDB").collection("users");
     const servicesCollection = client.db("parlourDB").collection("services");
